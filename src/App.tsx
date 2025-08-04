0a9953af59503320926d45b416aff5449099a253
BitSmith5 <121252319+BitSmith5@users.noreply.github.com>
2025-08-04 15:05:27 -0700

    Editing is now functional
    -Buttons are colored now

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import { TodoList } from "../src/components/TodoList";
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [text, setText] = useState<string>("");
+  const [editingId, setEditingId] = useState<string | null>(null);
 
   const addTodo = () => {
     if (text === "") return;
-    const newTodo: Todo = {
-      id: crypto.randomUUID(),
-      text: text,
-      completed: false,
-    };
-    setTodos((prev) => [...prev, newTodo]);
+    if (editingId) {
+      editTodo(editingId, text);
+      setEditingId(null);
+    }
+    else
+    {
+      const newTodo: Todo = {
+        id: crypto.randomUUID(),
+        text: text,
+        completed: false,
+      };
+      setTodos((prev) => [...prev, newTodo]);
+    }
+    setText("");
   };
 
   const toggleTodo = (id: string) => {
@@ -22,9 +31,13 @@ export default function App() {
     );
   };
 
-  const editTodo = (id: string) => {
+  const onEdit = (id: string ) => {
+    setEditingId(id);
+  }
+
+  const editTodo = (id: string, text: string) => {
     setTodos((prev) =>
-      prev.map((t) => (t.id === id ? { ...t, text: t.text } : t))
+      prev.map((t) => (t.id === id ? { ...t, text: text } : t))
     );
   };
 
@@ -33,19 +46,20 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Todo List</h1>
+    <div className="App bg-gray-600 text-white min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-4">Todo List</h1>
       <input
+        className="border p-2 rounded text-black"
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Enter your todo..."
       />
-      <button onClick={addTodo}>Add</button>
+      <button className="mx-3 hover:text-green-400" onClick={addTodo}>Add</button>
       <TodoList
         todos={todos}
         onToggle={toggleTodo}
-        onEdit={editTodo}
+        onEdit={onEdit}
         onDelete={deleteTodo}
       />
     </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,15 +9,15 @@ type TodoItemProps = {
 
 export function TodoItem({ todo, onToggle, onEdit, onDelete }: TodoItemProps) {
   return (
-    <li>
+    <li className="flex w-1/3 items-center justify-start gap-2 p-2 border-b border-gray-500">
       <input
         type="checkbox"
         checked={todo.completed}
         onChange={() => onToggle(todo.id)}
       />
       {todo.text}
-      <button onClick={() => onEdit(todo.id)}>Edit</button>
-      <button onClick={() => onDelete(todo.id)}>Delete</button>
+      <button className="text-yellow-400" onClick={() => onEdit(todo.id)}>Edit</button>
+      <button className="text-red-400" onClick={() => onDelete(todo.id)}>Delete</button>
     </li>
   );
 }
