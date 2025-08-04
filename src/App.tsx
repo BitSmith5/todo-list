ea2fbd63aa28d5073a4187683e9a5ed92f897b47
BitSmith5 <121252319+BitSmith5@users.noreply.github.com>
2025-08-04 16:08:29 -0700

    Removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Todo } from "../src/types";
 import { TodoList } from "../src/components/TodoList";
 
